4c76c287849ae46218fc3b95807aeabbf7ab8e6a
austinckohler <austinckohler@gmail.com>
2020-05-12 12:21:02 -0600

    intro to event listeners

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-fetch('http://bagel-api-fis.herokuapp.com/bagels')
+document.addEventListener('DOMContentLoaded', function(event) {
+  console.log('all content is loaded')
+
+const button = document.createElement('button')
+button.innerText = 'All bagels!'
+document.body.appendChild(button)
+
+button.addEventListener('click', (event) => {
+ fetchBagels()
+})
+
+function fetchBagels(){ 
+  fetch('http://bagel-api-fis.herokuapp.com/bagels')
   .then(response => response.json())
   .then(bagels => renderBagels(bagels))
- 
-
-
+}
 
 function renderBagels(bagels){
   const bagelList = document.getElementById('bagel-list')
@@ -11,6 +21,22 @@ function renderBagels(bagels){
   bagels.map(bagel => {
     const bagelLi = document.createElement('li')
     bagelLi.innerText = bagel.type
+    createDeleteButton(bagelLi)
     bagelList.appendChild(bagelLi)
   })
 }
+
+function createDeleteButton(bagelLi) {
+  const deleteButton = document.createElement('button')
+  deleteButton.innerText = 'delete'
+  bagelLi.appendChild(deleteButton)
+  deleteButton.addEventListener('click', (event) => { 
+    // event.target.parentNode.remove()
+    event.path[1].remove()
+  })
+}
+
+
+
+
+})
